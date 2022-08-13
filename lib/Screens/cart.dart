3fea934b9e1dac3dc13e9ff64448451aa0533522
Yan-0 <shreyanbhandari36@gmail.com>
2022-08-13 13:30:45 +0545

    Day 23 completed

--- lib/Screens/cart.dart
+++ lib/Screens/cart.dart
@@ -33,11 +33,14 @@ class _CartTotal extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          "\$${_cart.totalPrice}"
-              .text
-              .xl5
-              .color(context.theme.accentColor)
-              .make(),
+          VxBuilder(
+            mutations: {RemoveMutation},
+            builder: (context, store, status) => "\$${_cart.totalPrice}"
+                .text
+                .xl5
+                .color(context.theme.accentColor)
+                .make(),
+          ),
           30.widthBox,
           ElevatedButton(
             onPressed: () {
@@ -61,6 +64,7 @@ class _CartTotal extends StatelessWidget {
 class _CartList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    VxState.watch(context, on: [RemoveMutation]);
     final CartModel _cart = (VxState.store as MyStore).cart;
     return _cart.items.isEmpty
         ? "Nothing to show".text.xl3.makeCentered()
@@ -69,9 +73,7 @@ class _CartList extends StatelessWidget {
             itemBuilder: (context, index) => ListTile(
               leading: Icon(Icons.done),
               trailing: IconButton(
-                onPressed: () {
-                  _cart.remove(_cart.items[index]);
-                },
+                onPressed: () => RemoveMutation(_cart.items[index]),
                 icon: Icon(Icons.remove_circle_outline),
               ),
               title: _cart.items[index].name.text.make(),
