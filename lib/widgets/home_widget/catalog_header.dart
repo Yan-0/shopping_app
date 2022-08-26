3e57987f976aef4715a0904ba4c66a1a88174d16
Shreyan <shreyanbhandari36@gmail.com>
2022-08-26 23:01:56 +0545

    -Changes made to app icon and name. -Suttle UI changes made.

--- lib/widgets/home_widget/catalog_header.dart
+++ lib/widgets/home_widget/catalog_header.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:velocity_x/velocity_x.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class CatalogHeader extends StatelessWidget {
   const CatalogHeader({Key? key}) : super(key: key);
@@ -9,8 +10,20 @@ class CatalogHeader extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        "Catalog App".text.xl6.bold.color(context.accentColor).make(),
-        "Trending products".text.xl2.make(),
+        Row(children: [
+          "assets/images/S_logo.png"
+              .circularAssetImage(radius: 30.0, bgColor: Colors.black)
+              .innerShadow(color: Colors.black),
+          "hopmart"
+              .text
+              .xl6
+              .bold
+              .color(context.accentColor)
+              .textStyle(
+                  TextStyle(fontFamily: GoogleFonts.spaceGrotesk().fontFamily))
+              .make(),
+        ]),
+        "Trending products".text.xl3.make(),
       ],
     );
   }
